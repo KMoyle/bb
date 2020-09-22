f08462e8aad8d28027bc1862bf66e06655373978
Kyle <kylesmoyle@gmail.com>
2020-09-22 15:07:16 +1000

    fix

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/kyle/baebot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/kyle/gazebo_ws/devel;/home/kyle/baebot_ws/devel;/home/kyle/cartographer_ws/devel;/home/kyle/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/kyle/gazebo_ws/devel;/home/kyle/baebot_ws/devel;/home/kyle/cartographer_ws/devel;/home/kyle/catkin_ws/devel;/opt/ros/melodic' != '' else []
