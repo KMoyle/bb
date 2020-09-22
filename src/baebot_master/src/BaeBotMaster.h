8fe2696201b8c933a07e2633249334304c9b0f5a
Kyle <kylesmoyle@gmail.com>
2020-09-22 14:49:59 +1000

    Added a BB Gazebo robot and world, bb subs to cmd_vel

--- src/baebot_master/src/BaeBotMaster.h
+++ src/baebot_master/src/BaeBotMaster.h
@@ -40,42 +40,45 @@ class BaeBotMaster {
 
     public:
 
+    // Points to check control and senors data
     std::vector<std::pair<double, double> > goto_points;
-    ros::Time goto_time;
+    //Motor cmds to publish
+    std::pair<double, double> motor_cmds_vw;
 
+    // USING DEBUG & SIM MODES
+    bool SIM = false;
     bool DEBUG = true;
+
+    //Initial movement bool
     bool we_are_off = false;
+    //Threshold dist for Proportional control
     const double THRESH_DIST = 0.05; //5cm
 
-
+    //Declaring state and control objects
     BaeBotControl baeBotControl;
     MISSION_MODE mission_status;
     SENSOR_STATUS sensor_status;
 
-
     //Sensors timeouts
-    double sensorTimeOut;
+    double sensorTimeOut = 5.0;
     double timeSinceLastLidarUpdate;
     double timeSinceLastCameraUpdate;
     double timeSinceLastPoseUpdate;
 
-    std::pair<double, double> motor_cmds_vw;
-
+    // Timing infos
     double sampleRate = 30;
     ros::Duration dt;
     ros::Rate r = 30; //10Hz
     ros::Time* lastUpdateTime = NULL;
 
+    //Declaring pose & posedmd infos
     POSE        pose;
     POSE        poseDmd;
     POSE        poseGoal;
     Point2D     point_xy;
 
-
     //Pubs
-    ros::Publisher rviz_pub;
     ros::Publisher pose_pub;
-    ros::Publisher poseDmd_pub;
     ros::Publisher motorDmd_pub;
 
 
@@ -86,16 +89,13 @@ class BaeBotMaster {
     // SETS
     void setNewPose_xy( Point2D );
     void setNewPose( POSE );
-    void setVelocity( );
 
     // GETS
 
     // CALLBACK FUNCTIONS
     void rpLidarCallback(const sensor_msgs::LaserScan::ConstPtr& );
-    //void rpLidarCallback(const sensor_msgs::MultiEchoLaserScan::ConstPtr& );
     void cameraImageCallback(const sensor_msgs::Image::ConstPtr& );
     void bbPoseCallback(const nav_msgs::Odometry::ConstPtr& );
-    void bbPoseDmdCallback(const nav_msgs::Odometry::ConstPtr& );
     void batteryStateCallback( const sensor_msgs::BatteryState::ConstPtr& );
 
 
@@ -113,7 +113,6 @@ class BaeBotMaster {
     //DATA gathering functions to TEST ODOM
     void doASpin();
     void goStraight();
-
 
     // UTILS
     double dist_to_point( Point2D );
--- src/baebot_master/src/baebotMasterMain.cpp
+++ src/baebot_master/src/baebotMasterMain.cpp
@@ -30,13 +30,19 @@ int main( int argc, char **argv){
     baebotMaster->goto_points.push_back(P3);
     baebotMaster->goto_points.push_back(P2);
     baebotMaster->goto_points.push_back(P1);
+<<<<<<< refs/remotes/origin/master
 
+=======
+>>>>>>> Added a BB Gazebo robot and world, bb subs to cmd_vel
     baebotMaster->goto_points.push_back(P5);
     baebotMaster->goto_points.push_back(P4);
     baebotMaster->goto_points.push_back(P3);
     baebotMaster->goto_points.push_back(P2);
     baebotMaster->goto_points.push_back(P1);
+<<<<<<< refs/remotes/origin/master
 
+=======
+>>>>>>> Added a BB Gazebo robot and world, bb subs to cmd_vel
 
 
 
